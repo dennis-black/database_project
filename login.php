dcbf4aa3a71f8e2935a62ce46af7e5eaa6c88ae9
dennis-black <d3nni581ack@gmail.com>
2024-03-29 17:16:06 +0800

    add a template

--- login.php
+++ login.php
@@ -1,153 +1,145 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>黑貓工作室-登入</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
+    <meta charset="utf-8">
+    <title>丹尼斯的保鮮盒</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-        .logo {
-            height: 40px;
-            margin: 7px;
-        }
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
 
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 1rem;
-        }
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet"> 
 
-        nav ul {
-            float: right;
-            list-style: none;
-            display: flex;
-        }
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-        nav li {
-            margin-right: 1rem;
-        }
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
-        /* hover 導航欄效果 */
-        li a:hover{ 
-            background-color: #fff;
-            border-radius: 4px;
-            color: #736f6f;
-        }
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-        nav a {
-            text-decoration: none;
-            color: #fff;
-            font-size: 16px;
-        }
-
-        section {
-            padding: 2rem;
-        }
-
-        #home {
-            background-color: #f0f0f0;
-        }
-
-        #portfolio .portfolio-item {
-            margin-bottom: 2rem;
-        }
-
-        footer {
-            /* position: ;
-            bottom: 0;
-            width: 100%; */
-            /* left: 0; */
-            padding: 1rem;
-            text-align: center;
-            background-color: #333;
-            color: #fff;
-        }
-
-        @media screen and (max-width: 600px) {
-            header {
-                background-color: #333;
-                color: #fff;
-                padding-bottom: 60px;
-                /* height: 50px; */
-            }
-            nav a{
-                text-decoration: none;
-                color: #fff;
-                font-size: 14px;
-            }
-        }
-    </style>
-    <?php
-    session_start();
-    if ($_SERVER['REQUEST_METHOD'] === "POST"){
-        include "database_connection.php";
-        $username = $_POST['username']?? '';
-        $password = $_POST['password']?? '';
-
-        $stmt = $db->prepare("SELECT * FROM users WHERE username = :username");
-        $stmt->bindParam(':username', $username);
-        $stmt->execute();
-
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($user && password_verify($password, $user['password'])) {
-            $_SESSION['user_id'] = $user['id']; 
-            $_SESSION['username'] = $user['username']; 
-    
-            header('Location: menu.php'); 
-            exit;
-        } else {
-            // 登录失败，显示错误信息
-            echo "<script>alert('使用者名稱或密碼錯誤');</script>";
-        }
-    }
-    ?>
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
 </head>
-<body>
-    <header>
-    <nav>
-        <a href="https://dennis-black.github.io/website/index.html"><img class="logo" src="./assets/blackcatstudio_banner.png"></a>
-        <ul>
-        <li><a href="index.html">回首頁</a></li>
-        <li><a href="index.html" style="color: #99c2ff; font-weight: bolder;">登入</a></li>
-        </ul>
-    </nav>
-    </header>
-    <section id="home">
-    <h1>黑貓工作室 </h1>
-    <h2>Black Cat Studio</h2>
-    </section>
 
-    <section id="intro">
-    <h2>使用者登入</h2>
-        <form action="login.php" method="post" autocomplete="off">
-        <table>
-            <tr>
-            <td><p>使用者ID</p></td>
-            <td><input type="text" name="username" placeholder="輸入使用者ID"></td>
-            </tr>
-            <tr>
-            <td><p>密碼</p></td>
-            <td><input type="password" name="password" placeholder="輸入密碼"></td>
-            </tr>
-            <tr>
-            <td><input type="submit" name="login-btn" value="登入"></td>
-            <!-- <td><a href="#">忘記密碼？</a></td> -->
-            </tr>
-            <tr>
-            <td colspan = "2"><a href = "register.php">沒有帳號嗎？註冊一個吧</a></td>
-            </tr>
-        </table>
-        </form>        
-    </section>
-    <footer>
-    <p>&copy; 2023 黑貓工作室</p>
-    </footer>
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-light p-0 wow fadeIn" data-wow-delay="0.1s">
+        <div class="row gx-0 d-none d-lg-flex">
+            <div class="col-lg-7 px-5 text-start">
+                <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+                    <small class="fa fa-map-marker-alt text-primary me-2"></small>
+                    <small>阿拉伯聯合大公國阿布達比國際金融中心-科里弗蘭診所</small>
+                </div>
+                <div class="h-100 d-inline-flex align-items-center py-3">
+                    <small class="far fa-clock text-primary me-2"></small>
+                    <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
+                </div>
+            </div>
+            <div class="col-lg-5 px-5 text-end">
+                <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+                    <small class="fa fa-phone-alt text-primary me-2"></small>
+                    <small>+012 345 6789</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <h1 class="m-0 text-primary"><i class="far fa-hospital me-3"></i>丹尼斯的保鮮盒</h1>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">首頁</a>
+                <a href="about.html" class="nav-item nav-link">關於我們</a>
+            </div>
+            <a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">登入/註冊<i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Header Start -->
+    <div class="container-fluid header bg-primary p-0 mb-5">
+        
+    </div>
+    <!-- Header End -->
+
+
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-light mb-4">Address</h5>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>阿拉伯聯合大公國阿布達比國際金融中心-科里弗蘭診所</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" href="#">丹尼斯的保鮮盒</a>, All Right Reserved.
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,224 +1,145 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="styles.css">
-	<title>黑貓工作室-註冊</title>
-	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			margin: 0;
-			padding: 0;
-		}
+    <meta charset="utf-8">
+    <title>丹尼斯的保鮮盒</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-		.logo {
-			height: 40px;
-			margin: 7px;
-		}
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
 
-		header {
-			background-color: #333;
-			color: #fff;
-			padding: 1rem;
-		}
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet"> 
 
-		nav ul {
-			float: right;
-			list-style: none;
-			display: flex;
-		}
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-		nav li {
-			margin-right: 1rem;
-		}
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
-		/* hover 導航欄效果 */
-		li a:hover{ 
-			background-color: #fff;
-			border-radius: 4px;
-			color: #736f6f;
-		}
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-		nav a {
-			text-decoration: none;
-			color: #fff;
-			font-size: 16px;
-		}
-
-		section {
-			padding: 2rem;
-		}
-
-		#home {
-			background-color: #f0f0f0;
-		}
-
-		#portfolio .portfolio-item {
-			margin-bottom: 2rem;
-		}
-
-		footer {
-			/* position: ;
-			bottom: 0;
-			width: 100%; */
-			/* left: 0; */
-			padding: 1rem;
-			text-align: center;
-			background-color: #333;
-			color: #fff;
-		}
-
-		@media screen and (max-width: 600px) {
-			header {
-				background-color: #333;
-				color: #fff;
-				padding-bottom: 60px;
-				/* height: 50px; */
-			}
-			nav a{
-				text-decoration: none;
-				color: #fff;
-				font-size: 14px;
-			}
-		}
-
-	</style>
-	<?php
-		session_start();
-
-		if (!isset($_SESSION['csrf_token'])) $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-
-		include "database_connection.php";
-
-		if ($_SERVER['REQUEST_METHOD'] === "POST"){
-
-			if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) { //檢查cfrs令牌
-				die("CSRF token validation failed");
-			}
-
-			$username = $_POST['username']?? '';
-			$email = $_POST['email'] ?? '';
-			$password = $_POST['password'] ?? '';
-			$confirmPassword = $_POST['confirmPassword'] ?? '';
-			
-			$errors = '';
-			if (empty($username)) {
-				$errors .= "使用者ID不得為空\\n";
-			} else if (strlen($username) < 4 || strlen($username) > 50) {
-				$errors .= "使用者ID的長度必須至少4個字元且少於50個字元\\n";
-			}
-			
-			if (empty($email)) {
-				$errors .= "電子郵箱不得為空\\n";
-			} else if (strlen($email) < 4 || strlen($email) > 50) {
-				$errors .= "電子郵箱的長度必須至少4個字元且少於50個字元\\n";
-			}
-			
-			if (empty($password)) {
-				$errors .= "你的密碼不得為空\\n";
-			} else if (strlen($password) < 4 || strlen($password) > 50) {
-				$errors .= "密碼的長度必須至少4個字元且少於50個字元\\n";
-			}
-			
-			if (empty($confirmPassword)) {
-				$errors .= "再次輸入密碼不得為空\\n";
-			} else if ($password != $confirmPassword) {
-				$errors .= "你的密碼與再次確認密碼不同，請確保他們是相同的\\n";
-			} else if (strlen($confirmPassword) < 4 || strlen($confirmPassword) > 50) {
-				$errors .= "確認密碼的長度必須至少4個字元且少於50個字元\\n";
-			}
-
-			if(empty($errors)){
-				$checkUser = $db->prepare("SELECT COUNT(*) FROM users WHERE username = :username");
-				$checkUser -> bindParam(':username', $username);
-				$checkUser -> execute();
-
-				if($checkUser->fetchColumn() > 0) $errors.= "使用者名稱已經被註冊\\n";
-
-				$checkEmail = $db->prepare("SELECT COUNT(*) FROM users WHERE email = :email");
-				$checkEmail -> bindParam(':email', $email);
-				$checkEmail -> execute();
-
-				if($checkEmail->fetchColumn() > 0) $errors.= "電子郵箱已經被註冊\\n";
-			}
-			
-			if (!empty($errors)) echo "<script>alert('$errors');</script>";
-			else {
-				if (!empty($password)&&(strlen($password)>=4)&&(strlen($password)<=50)) {
-					$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-				}
-
-				try {
-					$stmt = $db->prepare("INSERT INTO users (role, username, email, password) VALUES (:role, :username, :email, :password)");
-					$role = 'user';
-					$stmt->bindParam(':role', $role);
-					$stmt->bindParam(':username', $username);
-					$stmt->bindParam(':email', $email);
-					$stmt->bindParam(':password', $hashedPassword);
-					$stmt->execute();
-				
-					echo "<script>
-							alert('使用者註冊成功');
-							setTimeout(function() {
-								window.location.href = 'login.php';
-							}, 3000);
-						</script>";
-				} catch (PDOException $e) {
-					echo "数据库错误: " . $e->getMessage();
-				}
-			}
-	
-		}
-	?>
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
 </head>
-<body>
-	<header>
-	<nav>
-		<a href=""><img class="logo" src="./assets/blackcatstudio_banner.png"></a>
-		<ul>
-			<li><a href="index.html">回首頁</a></li>
-			<li><a href="register.php" style="color: #99c2ff; font-weight: bolder;">註冊</a></li>
-		</ul>
-	</nav>
-	</header>
-	<section id="home">
-		<h1>黑貓工作室 </h1>
-		<h2>Black Cat Studio</h2>
-	</section>
 
-	<section id="intro">
-		<h2>使用者註冊</h2>
-		<form action="register.php" method="post" autocomplete="off">
-			<table>
-				<tr>
-					<td><p>使用者ID</p></td>
-					<td><input type="text" name="username" placeholder="輸入你的ID" maxlength="50"></td>
-				</tr>
-				<tr>
-					<td><p>電子郵箱</p></td>
-					<td><input type="email" name="email" placeholder="...@..." maxlength="50"></td>
-				</tr>
-				<tr>
-					<td><p>你的密碼</p></td>
-					<td><input type="password" name="password" placeholder="輸入你的密碼" maxlength="50"></td>
-				</tr>
-					<td><p>再次輸入密碼</p></td>
-					<td><input type="password" name="confirmPassword" placeholder="再次確認密碼" maxlength="50"></td>
-				</tr>
-				<tr>
-					<td colspan="2"><p><b>上述任一欄位字元數至少要4個字元且不得超過50個字元</b></p></td>
-				</tr>
-				<tr>
-					<td><input type="submit" name="registerBtn" value="註冊帳號"></td>
-					<td><input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>"></td>
-				</tr>
-			</table>
-		</form>
-	</section>
-	<footer>
-	<p>&copy; 2023 黑貓工作室</p>
-	</footer>
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-light p-0 wow fadeIn" data-wow-delay="0.1s">
+        <div class="row gx-0 d-none d-lg-flex">
+            <div class="col-lg-7 px-5 text-start">
+                <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+                    <small class="fa fa-map-marker-alt text-primary me-2"></small>
+                    <small>阿拉伯聯合大公國阿布達比國際金融中心-科里弗蘭診所</small>
+                </div>
+                <div class="h-100 d-inline-flex align-items-center py-3">
+                    <small class="far fa-clock text-primary me-2"></small>
+                    <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
+                </div>
+            </div>
+            <div class="col-lg-5 px-5 text-end">
+                <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+                    <small class="fa fa-phone-alt text-primary me-2"></small>
+                    <small>+012 345 6789</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <h1 class="m-0 text-primary"><i class="far fa-hospital me-3"></i>丹尼斯的保鮮盒</h1>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">首頁</a>
+                <a href="about.html" class="nav-item nav-link">關於我們</a>
+            </div>
+            <a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">登入/註冊<i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Header Start -->
+    <div class="container-fluid header bg-primary p-0 mb-5">
+        
+    </div>
+    <!-- Header End -->
+
+
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-light mb-4">Address</h5>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>阿拉伯聯合大公國阿布達比國際金融中心-科里弗蘭診所</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" href="#">丹尼斯的保鮮盒</a>, All Right Reserved.
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 </body>
+
 </html>
